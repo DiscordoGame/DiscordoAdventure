1d74b4c41b32ba11722fd2d0ba4954a3c01b4114
Mateusz Radomski <radomski.main@protonmail.com>
2021-03-13 16:12:58 +0100

    You can do a basic chat with the bot in the DM channel.

--- main.py
+++ main.py
@@ -10,11 +10,22 @@ class MyClient(discord.Client):
             return
 
         # We only accept messages from TextChannels
-        if isinstance(message.content, discord.TextChannel) and message.content == '!start':
+        if isinstance(message.channel, discord.TextChannel) and message.content == '!start':
             # Delete the message that the user sent with '!start' in the general channel
             await message.delete();
             # DM the author of message with a greating
             await message.author.send('Hello traveler ' + message.author.name + ".")
+        elif isinstance(message.channel, discord.DMChannel):
+            # Should split on regex with multiple spaces
+            parts = message.content.strip().split(' ')
+            if len(parts) == 1:
+                response = "What " + parts[0] + "??"
+            else:
+                command = parts[0]
+                response = "You really want to \"" + command + "\" " + parts[1] + "?"
+
+            await message.author.send(response)
+                
 
 client = MyClient()
 client.run(open('token.txt', 'r').read())
