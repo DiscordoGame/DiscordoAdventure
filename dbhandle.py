0ed9eee143452099f968529ff976ea5b9670adb8
Mateusz Radomski <radomski.main@protonmail.com>
2021-03-14 16:59:20 +0100

    A debugging database for players is born.

--- dbhandle.py
+++ dbhandle.py
@@ -12,5 +12,22 @@ class DatabaseHandle:
         if DatabaseHandle.handle == None:
             try:
                 DatabaseHandle.handle = _mysql.connect(host = host, user = user, passwd = passwd)
-            except:
-                print('Error connecting to database')
+            except Exception as e:
+                print('Error connecting to database: ' + str(e))
+
+            test_db_name = 'players'
+            try:
+                DatabaseHandle.handle.query('USE ' + test_db_name + ";")
+            except Exception as e:
+                print('Error setting the database we are going to use: ' + str(e))
+
+    def get_record_count(self, table_name):
+        # TODO(mateusz): Should set-up some rules how to guards
+        # against unwanted SQL injection attacks
+        # Anyway, this is stupid and is just here for debugging
+
+        sql = "SELECT * FROM " + table_name.strip() + ";"
+        DatabaseHandle.handle.query(sql)
+        res = DatabaseHandle.handle.store_result();
+
+        return res.num_rows()
