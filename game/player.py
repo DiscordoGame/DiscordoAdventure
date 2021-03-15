1a07ee13c4c0f374e33c40847e3cec9e2cfa5094
Mateusz Radomski <radomski.main@protonmail.com>
2021-03-15 21:40:08 +0100

    Bot sends a tutorial message if the player has not seen it.

--- game/player.py
+++ game/player.py
@@ -5,10 +5,21 @@ class Player:
         self.discord_id = discord_id
         self.login_date = ident_date
 
+    def seen_tutorial(self):
+        db = DatabaseHandle()
+
+        cmd = "SELECT seen_tutorial from players where discord_id = %s"
+        c = db.handle.cursor();
+        if c.execute(cmd, (self.discord_id,)):
+            res = c.fetchone()[0]
+            return res
+        else:
+            return False
+        
     def save_to_db(self):
         db = DatabaseHandle()
 
-        cmd = """INSERT INTO players VALUES(%s, %s, %s, 0) ON DUPLICATE KEY UPDATE last_msg_date = %s"""        
+        cmd = "INSERT INTO players VALUES(%s, %s, %s, 0) ON DUPLICATE KEY UPDATE last_msg_date = %s"
         c = db.handle.cursor();
         c.execute(cmd, (self.discord_id, self.login_date, self.login_date, self.login_date,))
         db.handle.commit();
--- main.py
+++ main.py
@@ -17,13 +17,31 @@ class MyClient(discord.Client):
         p = CmdParser(message.content)
         
         # We only accept messages from TextChannels
-        if isinstance(message.channel, discord.TextChannel) and p.is_command and p.get_command() == 'start':
-            # Delete the message that the user sent with '!start' in the general channel
-            await message.delete();
-            # DM the author of message with a greating
-            await message.author.send('Hello traveler ' + message.author.name + ".")
-            player = Player(message.author.id, message.created_at)
-            player.save_to_db()
+        if isinstance(message.channel, discord.TextChannel) and p.is_command:
+            if p.get_command() == 'start':
+                # Delete the message that the user sent with '!start' in the general channel
+                await message.delete();
+
+                author = message.author;
+                # DM the author of message with a greating
+                await author.send('Hello traveler ' + author.name + ".")
+
+                # TODO(mateusz): Should probably be read from some sort
+                # of a file where bot responses are stored to allow 
+                # maybe for translation and easier addition of lines
+                
+                player = Player(author.id, message.created_at)
+                if not player.seen_tutorial():
+                    tutorial = "To play the game send me two word commands with out the need for the prefixing '!'. Each command has a body like so <verb> <noun>, where verb is the thing you want to do and noun the thing you want to do it with. To get a list of all possible commands send a 'help commands' message."
+                    tutorial2 = "To practice your first command you can confirm to me that you understand everything and don't want to see this tutorial anymore. Simply type 'dismiss tutorial'..."
+                    await author.send(tutorial)
+                    await author.send(tutorial2)
+                
+                player.save_to_db()                
+            elif p.get_command() == 'help':
+                msg = "To start the game type !start and I will guide you through everything that you need to know."
+                
+                await message.channel.send(msg)
         elif isinstance(message.channel, discord.DMChannel):
             # Should split on regex with multiple spaces
             parts = message.content.strip().split(' ')
